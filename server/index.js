3c539e410242717e5b1198ea5b384a7b217bde8b
Jordan Pagni <jordan@softwebcity.com>
2020-09-10 16:42:46 -0700

    changed data for deployed proxy

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
+require('newrelic');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 3000;
+const port = 4003;
 
 app.use('/:id', express.static('public'));
 
-// Photo Carousel
-app.use('/api/photos/:restaurantID', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+// // Photo Carousel
+// app.use('/api/photos/:id', createProxyMiddleware({ target: 'http://13.57.187.79:3003', changeOrigin: true }));
 
-// Info Sidebar
-app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+// // Info Sidebar
+// app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://3.135.211.26/', changeOrigin: true }));
 
-// Tips & Recommendations
-app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
-app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
+// // Tips & Recommendations
+// app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://18.221.151.50:3003/', changeOrigin: true }));
+// app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://18.221.151.50:3003/', changeOrigin: true }));
 
-// Similar Restaurants
-app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+// // Similar Restaurants
+// app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://44.235.211.208:3004/', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
